aa38f3274fc3fdbe881a1bad9a382c76e9280c5c
EleSoyEle <angelgvsgm@gmail.com>
2024-11-16 02:33:19 -0600

    Primer commit del proyecto

---
+++ script.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+// Definimos las variables globales
+float m1 = 1;
+float m2 = 1;
+float l1 = 1;
+float l2 = 1;
+
+//Condiciones iniciales del pendulo doble
+float theta = 0;
+float dtheta = 0;
+
+float phi = 0;
+float dphi = 0;
+
+int main(){
+    printf("Todo bien");
+    return 0;
+}
